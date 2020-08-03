ac920f630a26fc953a76ebe06df8c81c8d53e804
Yannan <ytuo@berkeley.edu>
2020-08-03 11:39:48 -0700

    add non-virtual destructor tests

--- clang-tools-extra/test/clang-tidy/checkers/bsl-copy-move-access-specifier.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/bsl-copy-move-access-specifier.cpp
@@ -66,3 +66,49 @@ public:
 class H : public G {};
 // CHECK-MESSAGES: :[[@LINE-1]]:7: warning: Copy and move assignment operators shall be declared protected or defined “=delete” in base class; otherwise declare non-base class as final [bsl-copy-move-access-specifier]
 // CHECK-MESSAGES: :[[@LINE-2]]:7: warning: Copy and move constructors shall be declared protected or defined “=delete” in base class; otherwise declare non-base class as final [bsl-copy-move-access-specifier]
+
+// Abstract base class
+class I {
+public:
+  virtual void foo() = 0;
+  I(I const &) = default;
+  // CHECK-MESSAGES: :[[@LINE-1]]:3: warning: Copy and move constructors shall be declared protected or defined “=delete” in base class; otherwise declare non-base class as final [bsl-copy-move-access-specifier]
+  I(I &&) = default;
+  // CHECK-MESSAGES: :[[@LINE-1]]:3: warning: Copy and move constructors shall be declared protected or defined “=delete” in base class; otherwise declare non-base class as final [bsl-copy-move-access-specifier]
+  I &operator=(I const &) = default;
+  // CHECK-MESSAGES: :[[@LINE-1]]:6: warning: Copy and move assignment operators shall be declared protected or defined “=delete” in base class; otherwise declare non-base class as final [bsl-copy-move-access-specifier]
+  I &operator=(I &&) = default;
+  // CHECK-MESSAGES: :[[@LINE-1]]:6: warning: Copy and move assignment operators shall be declared protected or defined “=delete” in base class; otherwise declare non-base class as final [bsl-copy-move-access-specifier]
+};
+
+// Abstract base class
+class J {
+public:
+  virtual void foo() = 0;
+
+protected:
+  J(J const &) = default;
+  J(J &&) = default;
+  J &operator=(J const &) = default;
+  J &operator=(J &&) = default;
+};
+
+// Abstract base class
+class K {
+public:
+  virtual void foo() = 0;
+  K(K const &) = delete;
+  K(K &&) = delete;
+  K &operator=(K const &) = delete;
+  K &operator=(K &&) = delete;
+};
+
+
+// Final class, non-base
+class L final {
+public:
+  L(L const &) = default;
+  L(L &&) = default;
+  L &operator=(L const &) = default;
+  L &operator=(L &&) = default;
+};
